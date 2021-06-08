1c196571b92696c0cdd6b0df261c2bb1d87ea15d
Thorthur Planté <tuturtemple@gmail.com>
2021-06-08 22:37:19 +0200

    Update for compatibility with Flarum 0.1.0-beta.16

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,7 +1,65 @@
 import app from 'flarum/app';
 
-import KeycloakSettingsModal from './components/KeycloakSettingsModal';
+const settingsPrefix = 'spookygames-auth-keycloak.';
+const translationPrefix = 'flarum-ext-auth-keycloak.admin.keycloak_settings.';
 
 app.initializers.add('spookygames-auth-keycloak', app => {
-  app.extensionSettings['spookygames-auth-keycloak'] = () => app.modal.show(KeycloakSettingsModal);
+  app.extensionData
+    .for('spookygames-auth-keycloak')
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'server_url',
+        label: app.translator.trans(translationPrefix + 'server_url_label'),
+        type: 'text',
+        placeholder: 'https://keycloak.example.com/auth',
+      }
+    )
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'realm',
+        label: app.translator.trans(translationPrefix + 'realm_label'),
+        type: 'text',
+        placeholder: 'my_realm',
+      }
+    )
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'app_id',
+        label: app.translator.trans(translationPrefix + 'client_id_label'),
+        type: 'text',
+        placeholder: 'flarum',
+      }
+    )
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'app_secret',
+        label: app.translator.trans(translationPrefix + 'client_secret_label'),
+        type: 'text',
+        placeholder: '88a44c58-17c4-414c-85ed-4c56594131ba',
+      }
+    )
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'encryption_algorithm',
+        label: app.translator.trans(translationPrefix + 'encryption_algorithm_label'),
+        type: 'text',
+        placeholder: 'RS256',
+      }
+    )
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'encryption_key',
+        label: app.translator.trans(translationPrefix + 'encryption_key_label'),
+        type: 'text',
+        placeholder: '-----BEGIN PUBLIC KEY----- MIIBIjANsomethingIDAQAB -----END PUBLIC KEY-----',
+      }
+    )
+    .registerSetting(
+      {
+        setting: settingsPrefix + 'role_mapping',
+        label: app.translator.trans(translationPrefix + 'role_mapping_label'),
+        type: 'text',
+        placeholder: '{"ROLE_MEMBER":"Member","ROLE_MODERATOR":"Mods","ROLE_ADMIN":"Admin"}',
+      }
+    )
 });
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -55,7 +55,7 @@ app.initializers.add('spookygames-auth-keycloak', () => {
     extend(SettingsPage.prototype, 'settingsItems', function(items) {
         // Account management is mostly delegated to Keycloak
         // Better hide this from here
-        if (items.has('account')) {
+        if (items.has('account') && items.get('account').children.length === 0) {
             items.remove('account');
         }
     });
